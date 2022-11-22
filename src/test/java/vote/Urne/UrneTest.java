62dc19dacb4acea1259349122ed808352427c3ad
Elian Ducheyne <elian.ducheyne@gmail.com>
2022-11-22 15:23:38 +0100

    Correction test

--- src/test/java/vote/Urne/UrneTest.java
+++ src/test/java/vote/Urne/UrneTest.java
@@ -9,34 +9,6 @@ import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UrneTest {
-    @Test
-    void getSondageEnCoursConsigneEstUneConsigne() {
-        String consigne = "Une consigne";
-        Sondage sondageRecu = null;
-
-        Sondage sondage = new Sondage(consigne,"choix 1", "choix 2");
-        Urne urne = new Urne(5565);
-        try {
-            urne.lancerNouveauSondage(sondage);
-            Socket clientSimuler = new Socket("127.0.0.1",5565);
-            ObjectOutputStream outputData = new ObjectOutputStream(clientSimuler.getOutputStream());
-            ObjectInputStream inputData = new ObjectInputStream(clientSimuler.getInputStream());
-
-            outputData.writeUTF("getSondage");
-            outputData.flush();
-
-            sondageRecu = (Sondage) inputData.readObject();
-
-            urne.arreterSondage();
-        }
-        catch (Exception e){
-            System.out.println("error catched" + e);
-        }
-
-        assert sondageRecu != null;
-        System.out.println("Consigne recu: " + sondageRecu.getConsigne());
-        assertEquals(consigne,sondageRecu.getConsigne());
-    }
+class BureauDeVoteTest {
 
 }
