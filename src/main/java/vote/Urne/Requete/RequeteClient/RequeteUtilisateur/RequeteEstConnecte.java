5ca2efedc07a974cedc9d523db3c2de30f2c4dd4
Elian Ducheyne <elian.ducheyne@gmail.com>
2023-01-07 18:19:54 +0100

    requeteEstConnecte

--- src/main/java/vote/Urne/Requete/RequeteClient/RequeteUtilisateur/RequeteEstConnecte.java
+++ src/main/java/vote/Urne/Requete/RequeteClient/RequeteUtilisateur/RequeteEstConnecte.java
@@ -10,27 +10,21 @@ import vote.crypto.Hash;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-public class RequeteChangePassword extends Requete {
+public class RequeteEstConnecte extends Requete {
     private static final long serialVersionUID = -8228560866132091777L;
     private String ssid;
-    private String newPassword;
     //cela deconnectera la personne
-    public RequeteChangePassword(String newPassword, String ssid) {
-        super("changerMdp");
+    public RequeteEstConnecte(String ssid) {
+        super("est connecte");
         this.ssid = ssid;
-        this.newPassword = newPassword;
     }
 
     @Override
     public void repondre(BureauDeVote bureau, ObjectOutputStream out) throws IOException {
         if(ConnexionsHandler.getInstance().isConnected(ssid)){
-            Employe e = ConnexionsHandler.getInstance().getEmploye(ssid);
-            byte[][] hash = Hash.hashPassword(newPassword);
-            Employe m = new Employe(e.getEmail(),e.getNom(),e.getPrenom(),hash[0],hash[1], e.getIsAdmin());
-            EmployeManager.getInstance().mettreAJourEmploye(m);
-            out.writeObject("OK");
+            out.writeObject(true);
         }else {
-            out.writeObject("Pas les droits");
+            out.writeObject(false);
         }
         out.flush();
     }
